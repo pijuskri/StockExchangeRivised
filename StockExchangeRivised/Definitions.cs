a88dc5ecd995ec0a830e975d344f87d949962060
pijuskri <pijuskri@gmail.com>
2019-05-07 17:07:08 +0300

    initial

---
+++ StockExchangeRivised/Definitions.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockExchangeRivised
+{
+    public class Company
+    {
+        public string name;
+        public int totalShares, sharesOwnedByCompany;
+        public double money, dividendPercent, revenue, value, sharePrice, productionVolume, productionOutput, productionInput, actualProduction;
+        public string productionRecipe;
+        public List<Share> shareList;
+
+        public Company(string name, int totalShares, int sharesOwnedByCompany, double money, double dividendPercent, double revenue, double value, double sharePrice,
+            double productionVolume, double actualProduction, double productionOutput, double productionInput, string productionRecipe)
+        {
+            this.name = name;
+            this.totalShares = totalShares;
+            this.sharesOwnedByCompany = sharesOwnedByCompany;
+            this.money = money;
+            this.dividendPercent = dividendPercent;
+            this.revenue = revenue;
+            this.value = value;
+            this.sharePrice = sharePrice;
+            this.productionVolume = productionVolume;
+            this.actualProduction = actualProduction;
+            this.productionOutput = productionOutput;
+            this.productionInput = productionInput;
+            this.productionRecipe = productionRecipe;
+            shareList = new List<Share>();
+
+        }
+        public void AddShare(string ownerName, int amount)
+        {
+            foreach (var share in shareList) //check if already on list
+            {
+                if (share.ownerName == ownerName) { share.amount += amount; return; }
+            }
+            shareList.Add(new Share(ownerName, amount));
+        }
+    }
+    public class Resource
+    {
+        public string name, type;
+        public double price, amount, costToProduce, basePrice, flexibility, supply, demand;
+
+        public Resource(string name, string type, double price, double amount, double costToProduce, double basePrice, double flexibility, double supply, double demand)
+        {
+            this.name = name;
+            this.type = type;
+            this.price = price;
+            this.amount = amount;
+            this.costToProduce = costToProduce;
+            this.basePrice = basePrice;
+            this.flexibility = flexibility;
+            this.supply = supply;
+            this.demand = demand;
+        }
+    }
+    public class AI
+    {
+        public string name;
+        public double money, riskFactor, randomness;
+
+        public AI(string name, double money, double riskFactor, double randomness)
+        {
+            this.name = name;
+            this.money = money;
+            this.riskFactor = riskFactor;
+            this.randomness = randomness;
+        }
+    }
+    public class Share
+    {
+        public string ownerName;
+        public double amount;
+
+        public Share(string ownerName, double amount)
+        {
+            this.ownerName = ownerName;
+            this.amount = amount;
+        }
+    }
+    public class Bank
+    {
+        public string name;
+        public double interest, money, minimumCredit;
+
+        public Bank(string name, double interest, double money, double minimumCredit)
+        {
+            this.name = name;
+            this.interest = interest;
+            this.money = money;
+            this.minimumCredit = minimumCredit;
+        }
+    }
+    public class ShareSale
+    {
+        public string sellerName, company;
+        public double amount, price;
+
+        public ShareSale(string sellerName, string company, double amount, double price)
+        {
+            this.sellerName = sellerName;
+            this.company = company;
+            this.amount = amount;
+            this.price = price;
+        }
+    }
+    public class Loan
+    {
+        public string company, bank;
+        public double totalMoneyLent, interest, timeToPay, moneyPaidBack;
+
+        public Loan(string company, string bank, double totalMoneyLent, double interest, double timeToPay, double moneyPaidBack)
+        {
+            this.company = company;
+            this.bank = bank;
+            this.totalMoneyLent = totalMoneyLent;
+            this.interest = interest;
+            this.timeToPay = timeToPay;
+            this.moneyPaidBack = moneyPaidBack;
+        }
+    }
+    public class PopulationDemand
+    {
+        public string name;
+        public double amountPerHuman;
+
+        public PopulationDemand(string name, double amountPerHuman)
+        {
+            this.name = name;
+            this.amountPerHuman = amountPerHuman;
+        }
+    }
+    public class ResourceAmount
+    {
+        public string name;
+        public double amount;
+
+        public ResourceAmount(string name, double amount)
+        {
+            this.name = name;
+            this.amount = amount;
+        }
+    }
+    public class ProductionRecipe
+    {
+        public string name;
+        public List<ResourceAmount> input;
+        public List<ResourceAmount> output;
+
+        public ProductionRecipe(string name, List<ResourceAmount> input, List<ResourceAmount> output)
+        {
+            this.name = name;
+            this.input = input;
+            this.output = output;
+        }
+    }
+    public class HumanPlayer
+    {
+        public double money;
+
+        public HumanPlayer(double money)
+        {
+            this.money = money;
+        }
+    }
+    public class ResourceSaleCategory
+    {
+        public string name;
+        public List<ResourceSale> sales;
+        public ResourceSaleCategory(string name)
+        {
+            this.name = name;
+        }
+    }
+    public class ResourceSale
+    {
+        public double amount;
+        public double price;
+        public string company;
+        public ResourceSale(double amount, double price, string company)
+        {
+            this.amount = amount;
+            this.price = price;
+            this.company = company;
+        }
+    }
+}
---
+++ StockExchangeRivised/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace StockExchangeRivised
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nextTurn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // nextTurn
+            // 
+            this.nextTurn.Location = new System.Drawing.Point(106, 127);
+            this.nextTurn.Name = "nextTurn";
+            this.nextTurn.Size = new System.Drawing.Size(75, 23);
+            this.nextTurn.TabIndex = 0;
+            this.nextTurn.Text = "Next turn";
+            this.nextTurn.UseVisualStyleBackColor = true;
+            this.nextTurn.Click += new System.EventHandler(this.nextTurn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.nextTurn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button nextTurn;
+    }
+}
+
